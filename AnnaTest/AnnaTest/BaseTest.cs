30ba426e83ba6dd3022bc69cff655d3192792497
Oleh Dubishchev <Oleh.Dubishchev@infopulse.com>
2023-09-12 19:05:48 +0300

    aaaaa

---
+++ AnnaTest/AnnaTest/BaseTest.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+using NUnit.Framework.Interfaces;
+using Allure.Commons;
+using NUnit.Allure.Core;
+
+namespace AnnaTest
+{
+    [AllureNUnit]
+    public class BaseTest
+    {
+
+        protected IWebDriver webDriver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl("http://10.7.28.18:86");
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                var screenshot = ((ITakesScreenshot)webDriver).GetScreenshot();
+                var filename = TestContext.CurrentContext.Test.MethodName + "_screenshot_" + DateTime.Now.Ticks + ".png";
+                var path = "D:\\" + filename;
+                screenshot.SaveAsFile(path, ScreenshotImageFormat.Png);
+                TestContext.AddTestAttachment(path);
+                AllureLifecycle.Instance.AddAttachment(filename, "image/png", path);
+
+            }
+            webDriver.Close();
+            webDriver.Quit();
+        }
+
+        protected void ClickOnElement(IWebElement webElement)
+        {
+            if (webElement.Displayed)
+            {
+                webElement.Click();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        protected void EnterTextToElement(IWebElement webElement, string textToElement)
+        {
+            webElement.Click();
+            webElement.Clear();
+            webElement.SendKeys(textToElement);
+        }
+
+    }
+}
---
+++ AnnaTest/AnnaTest/SimpleTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Allure.Attributes;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace AnnaTest
+{
+    public class SimpleTest : BaseTest
+    {
+
+
+
+        private IWebElement franceButton => webDriver.FindElement(By.XPath("//a[contains(text(),'fr_team')]"));
+        private IWebElement userName => webDriver.FindElement(By.Id("UserName"));
+        private IWebElement passWord => webDriver.FindElement(By.Id("Password"));
+        private IWebElement loginButton => webDriver.FindElement(By.XPath("//button[contains(text(),'Log in')]"));
+
+
+        [Test]
+        [AllureSuite("suite name")]
+        public void OpenSXATest()
+        {
+            ClickOnElement(franceButton);
+            EnterTextToElement(userName, "super");
+            EnterTextToElement(passWord, "brmv3");
+            ClickOnElement(loginButton);
+
+            Assert.That(webDriver.Title, Is.EqualTo("Home Page"));
+
+
+        }
+
+    }
+}
